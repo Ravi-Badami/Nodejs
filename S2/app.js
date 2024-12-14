e27ea09bed2639162ea1558e71eb14cac9c8c009
Ravi-Badami <ravibadami191@gmail.com>
2024-12-14 19:05:16 +0530

    route handlers

--- S2/app.js
+++ S2/app.js
@@ -2,9 +2,78 @@ const express = require('express');
 const app = express();
 app.listen(3000);
 
-app.use('/test', (req, res) => {
-  res.send('hello');
+app.get('/user-:id-:name', (req, res) => {
+  res.send(req.params);
 });
-app.use('/', (req, res) => {
+
+/*
+ * Anything come before fly word will work and fly should be at the last.
+ * A word should end with fly
+ */
+
+app.get(/.*fly$/, (req, res) => {
+  res.send('fly is working');
+});
+
+/*
+ * Anything that contains a works
+ */
+app.get(/a/, (req, res) => {
+  res.send('/a/ is working');
+});
+/*
+ * similar to ab?c but here its (bc) both together
+ */
+app.get('/a(bc)?de', (req, res) => {
+  res.send('a(bc)?de working');
+});
+
+/*
+ * abc or abRANDOMc. Because anything can come between ab and c.but abc must be complsury present
+ * in the order or a b c.
+ */
+app.get('/ab*c', (req, res) => {
+  res.send('ab*c working');
+});
+
+/*
+ * abc or abbbc. Because b char can occur any number of times
+ */
+app.get('/ab+c', (req, res) => {
+  res.send('ab+c working');
+});
+/*
+ * Either ac or abc because the char on which ? is applied is optional
+ */
+app.get('/ab?c', (req, res) => {
+  res.send('ab?c working');
+});
+
+app.post('/', (req, res) => {
+  res.send('sendig love');
+});
+
+app.use('/test', (req, res) => {
   res.send('testing');
 });
+
+/*
+ * One route can have multiple route handlers */
+app.use(
+  '/:userId/:country/:age',
+  (req, res, next) => {
+    /*
+     * If you dont send the response at the end then the client
+     * will keep requesting and it hangs around until the timeout is hit*/
+    console.log('First callback');
+    next();
+
+    res.send(req.params);
+    next();
+  },
+  (req, res) => {
+    console.log('Second callback');
+
+    // res.send('second one');
+  }
+);
--- S2/homework.md
+++ S2/homework.md
@@ -9,3 +9,14 @@
 - what are dependencies
 - what is the use of "-g" while npm install
 - Difference between caret and tilde( ^ vs ~ )
+
+- initialize git
+- gitignore
+- create a remote repo on github
+- push all code to remote origin
+- play with routes and route extensions ex. /hello. /, hello/2, /xyz
+- order of the routes matter a lot
+- install postman app and make a workspace/collectio > test API call
+- write logic to handle GET ,POST, PATCH, DELETE API calls and test the on postman
+
+
